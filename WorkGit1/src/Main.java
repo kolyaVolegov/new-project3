7147781efa465ab35a87447e631f0c43af58cfd8
Nikolay Volegov <kolyavolegov@gmail.com>
2023-04-03 09:54:36 +0300

    New project 03.04.2023

---
+++ WorkGit1/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет дорогие друзья");
+        System.out.println("Вот, я пришел к вам)))");
+    }
+}
